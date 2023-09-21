adb96eefeb09e932916e0e0a1bd5938064a39a0a
vilasanh <vls2016.v3@gmail.com>
2023-09-21 10:01:52 +0700

    first upload

---
+++ resources/js/app.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <h1>Hello VueJS3</h1>
+</div>
+</template>
+
+<script>
+export default {
+name:'JsanhShopApp',
+data(){
+    return{
+
+    };
+},
+mounted(){
+
+},
+methods:{
+
+},
+};
+</script>
+
+<style lang = "scss" scoped >
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+ //   return view('app');
+//});
+Route::get('{any}', function () {
+    return view('app');
+})->where('any','.*');
